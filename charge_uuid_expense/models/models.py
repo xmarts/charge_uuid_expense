e0ede284eca7586b1b42e5402af334e32c7a2678
Pablo Osorio Gama <pablo.osorio@xmarts.com>
2019-07-04 14:07:48 -0500

    update_module

--- charge_uuid_expense/models/models.py
+++ charge_uuid_expense/models/models.py
@@ -1,15 +1,46 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+import tempfile
+import base64
+import os
+from lxml import etree, objectify
+from xml.etree import ElementTree as ET
 
-# class charge_uuid_expense(models.Model):
-#     _name = 'charge_uuid_expense.charge_uuid_expense'
+class HRExpense(models.Model):
+	_inherit = "hr.expense"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+	xml_file = fields.Binary(string="Archivo XML")
+	xml_filename = fields.Char('File Name')
+	xml_uuid = fields.Char(string="Folio Fiscal")
+
+	@api.onchange('xml_file')
+	def onchange_read_xml_uuid(self):
+		if self.xml_file:
+			result = self.xml_file.encode('utf-8')
+			data = base64.decodestring(result)
+			fobj = tempfile.NamedTemporaryFile(delete=False)
+			fname = fobj.name
+			fobj.write(data)
+			fobj.close()
+			file_xml = open(fname,"r")
+			tree = objectify.fromstring(file_xml.read().encode())
+			if self._get_stamp_data(tree) == None:
+				self.xml_uuid = 'Error en archivo XML'
+			else:
+				tfd = self._get_stamp_data(tree)
+				self.xml_uuid = tfd.get('UUID')
+		else: 
+			self.xml_uuid = 'XML no cargado'
+
+
+
+	@api.model
+	def _get_stamp_data(self, cfdi):
+		self.ensure_one()
+		if not hasattr(cfdi, 'Complemento'):
+			return None
+		attribute = 'tfd:TimbreFiscalDigital[1]'
+		namespace = {'tfd': 'http://www.sat.gob.mx/TimbreFiscalDigital'}
+		node = cfdi.Complemento.xpath(attribute, namespaces=namespace)
+		return node[0] if node else None
