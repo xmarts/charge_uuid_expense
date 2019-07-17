f5f399e53699b3c601e5fd7b986fdfbec5356157
soto <Hz95EuA5A>
2019-07-17 12:19:26 -0500

    correcion ..
    ...

--- charge_uuid_expense/models/models.py
+++ charge_uuid_expense/models/models.py
@@ -13,7 +13,9 @@ class HRExpense(models.Model):
 
 	xml_file = fields.Binary(string="Archivo XML")
 	xml_filename = fields.Char('File Name')
-	xml_uuid = fields.Char(string="Folio Fiscal")
+	xml_uuid_r = fields.Char(string='Almacenamiento del valor')
+	xml_uuid = fields.Char(string="Folio Fiscal", related="xml_uuid_r", readonly=True)
+	
 
 	@api.onchange('xml_file')
 	def onchange_read_xml_uuid(self):
@@ -27,19 +29,19 @@ class HRExpense(models.Model):
 			file_xml = open(fname,"r")
 			tree = objectify.fromstring(file_xml.read().encode())
 			if self._get_stamp_data(tree) == None:
-				self.xml_uuid = 'Error en archivo XML'
+				self.xml_uuid_r = 'Error en archivo XML'
 			else:
 				tfd = self._get_stamp_data(tree)
 				xml = tfd.get('UUID')
-				entrada_stock =self.env['hr.expense'].search([('xml_uuid', '=', xml)])
-				if entrada_stock:
+				entrada =self.env['hr.expense'].search([('xml_uuid_r', '=', xml)])
+				if entrada:
 					raise UserError(_('Este XMl ya fue utilizado'))					
 				else:
 					tfd = self._get_stamp_data(tree)
-					self.xml_uuid = tfd.get('UUID')
+					self.xml_uuid_r = tfd.get('UUID')
 					
 		else: 
-			self.xml_uuid = 'XML no cargado'
+			self.xml_uuid_r = 'XML no cargado'
 
 
 
