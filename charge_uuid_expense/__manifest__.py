2529bba26cbe8e94f6d4999aae4d1f3a40a2c1dd
Pablo Osorio <pablo.osorio@xmarts.com>
2019-08-13 13:06:40 -0500

    update

--- charge_uuid_expense/__manifest__.py
+++ charge_uuid_expense/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "charge_uuid_expense",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Agrega campo para uuid en el registro de gastos""",
 
     'description': """
-        Long description of module's purpose
+        
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Xmarts",
+    'website': "https://xmarts.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
